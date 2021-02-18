153bc5965a7f8a0a3f84c7dda9f162f0d2ea1132
eespunes <erik.espunyes7@outlook.com>
2021-02-18 20:30:54 +0100

    Problem 1 DONE

--- Level 01/Problem001.cpp
+++ Level 01/Problem001.cpp
@@ -14,9 +14,17 @@ int main()
     std::cout << "\t\tFind the sum of all the multiples of 3 or 5 below 1000." << std::endl;
     std::cout << std::endl;
 
-    float answer{0};
+    int answer{0};
 
     //YOUR CODE HERE
+
+    int input{0};
+    
+    for (int i = 1; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            answer += i;
+    }
 
     std::cout << "ANSWER: " << answer << std::endl;
 
