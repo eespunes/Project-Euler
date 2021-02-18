1364e13d4630a44b70d89e9fbe9082d073bada94
eespunes <erik.espunyes7@outlook.com>
2021-02-18 21:20:00 +0100

    Problem 4 DONE

--- Level 01/Problem004.cpp
+++ Level 01/Problem004.cpp
@@ -3,20 +3,44 @@
 int main()
 {
     std::cout << "####### ###" << std::endl;
-    std::cout << "Problem 001" << std::endl;
+    std::cout << "Problem 004" << std::endl;
     std::cout << "####### ###" << std::endl;
     std::cout << std::endl;
-    std::cout << "\t--------- -- - --- -" << std::endl;
-    std::cout << "\tMultiples of 3 and 5" << std::endl;
-    std::cout << "\t--------- -- - --- -" << std::endl;
+    std::cout << "\t------- ---------- -------" << std::endl;
+    std::cout << "\tLargest palindrome product" << std::endl;
+    std::cout << "\t------- ---------- -------" << std::endl;
     std::cout << std::endl;
-    std::cout << "\t\tIf we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23." << std::endl;
-    std::cout << "\t\tFind the sum of all the multiples of 3 or 5 below 1000." << std::endl;
+    std::cout << "\t\tA palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99." << std::endl;
+    std::cout << "\t\tFind the largest palindrome made from the product of two 3-digit numbers." << std::endl;
     std::cout << std::endl;
 
     float answer{0};
 
     //YOUR CODE HERE
+    bool isPalindromic = true;
+
+    for (int i = 1; i < 1000; i++)
+    {
+        for (int j = 1; j < 1000; j++)
+        {
+            int possiblePalindromic = i * j;
+            std::string possiblePalindromicString = std::to_string(possiblePalindromic);
+            isPalindromic = true;
+            for (int k = 0; k < possiblePalindromicString.length(); k++)
+            {
+                if (possiblePalindromicString[k] != possiblePalindromicString[possiblePalindromicString.length() - 1 - k])
+                {
+                    isPalindromic = false;
+                    break;
+                }
+            }
+            if (isPalindromic)
+            {
+                if (possiblePalindromic > answer)
+                    answer = possiblePalindromic;
+            }
+        }
+    }
 
     std::cout << "ANSWER: " << answer << std::endl;
 
