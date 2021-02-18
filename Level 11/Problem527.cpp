b40e4ca69895064833febda08943de411e21569b
eespunes <erik.espunyes7@outlook.com>
2021-02-18 12:07:16 +0100

    Files Created

---
+++ Level 11/Problem527.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "####### ###" << std::endl;
+    std::cout << "Problem 001" << std::endl;
+    std::cout << "####### ###" << std::endl;
+    std::cout << std::endl;
+    std::cout << "\t--------- -- - --- -" << std::endl;
+    std::cout << "\tMultiples of 3 and 5" << std::endl;
+    std::cout << "\t--------- -- - --- -" << std::endl;
+    std::cout << std::endl;
+    std::cout << "\t\tIf we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23." << std::endl;
+    std::cout << "\t\tFind the sum of all the multiples of 3 or 5 below 1000." << std::endl;
+    std::cout << std::endl;
+
+    float answer{0};
+
+    //YOUR CODE HERE
+
+    std::cout << "ANSWER: " << answer << std::endl;
+
+    return 0;
+}
